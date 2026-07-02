cb78284391256473cb24bf669eb603d0afa0396b
Ayme <dev.ayme99@gmail.com>
2026-07-02 11:44:54 +0200

    Fix: completar CRUD de mazos en controller

--- src/controllers/deckController.js
+++ src/controllers/deckController.js
@@ -1,3 +1,14 @@
+const Deck = require('../models/Deck');
+
+exports.getDecks = async (req, res) => {
+  try {
+    const decks = await Deck.find({ userId: req.query.userId });
+    res.json(decks);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
 exports.getDeckById = async (req, res) => {
   try {
     const deck = await Deck.findById(req.params.id);
@@ -5,6 +16,16 @@ exports.getDeckById = async (req, res) => {
     res.json(deck);
   } catch (error) {
     res.status(500).json({ error: error.message });
+  }
+};
+
+exports.createDeck = async (req, res) => {
+  try {
+    const deck = new Deck(req.body);
+    await deck.save();
+    res.status(201).json(deck);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
   }
 };
 
@@ -28,5 +49,16 @@ exports.deleteDeck = async (req, res) => {
     res.json({ message: 'Mazo eliminado correctamente' });
   } catch (error) {
     res.status(500).json({ error: error.message });
+  }
+};
+
+exports.updateDeckStats = async (req, res) => {
+  try {
+    const { result } = req.body;
+    const update = result === 'win' ? { $inc: { wins: 1 } } : { $inc: { losses: 1 } };
+    const deck = await Deck.findByIdAndUpdate(req.params.id, update, { new: true });
+    res.json(deck);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
   }
 };
