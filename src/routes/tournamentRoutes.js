f60711f0d8cb4605abd8258bb1cd0dfed58ad3b9
Ayme <dev.ayme99@gmail.com>
2026-07-11 10:19:39 +0200

    Añadido standings a las rutas del torneo

--- src/routes/tournamentRoutes.js
+++ src/routes/tournamentRoutes.js
@@ -10,6 +10,7 @@ router.get('/:id', tournamentController.getTournamentById);
 router.post('/', tournamentController.createTournament);
 router.put('/:id', tournamentController.updateTournament);
 router.delete('/:id', tournamentController.deleteTournament);
+router.post('/:id/standing', tournamentController.addStandingSnapshot);
 router.get('/:id/summary', tournamentController.getTournamentSummary);
 
 module.exports = router;
